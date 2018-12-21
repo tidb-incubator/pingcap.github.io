13ade8b4ec07ca4d1c29ee21b3888290e5d18d70
Yini Xu <34967660+YiniXu9506@users.noreply.github.com>
2018-12-21 18:02:18 +0800

    Yini/add copy (#127)
    * add copy to clipboard
    
    * update copy-to-clipboard
    
    * *: revert preview
    
    * *: remove submodule content/academy
    
    * requirements.txt: add python dependences for nelify build env
    
    * requirements.txt: update python dependences
    
    * Yini/add copy (#110)
    
    * add copy to clipboard
    
    * update copy-to-clipboard
    
    * add vendor javascript: clipboard.js
    
    * Yini/add copy (#112)
    
    * add copy to clipboard
    
    * update copy-to-clipboard
    
    * add vendor javascript: clipboard.js
    
    * add console.log for debugging
    
    * remove clipboardjs from app.js
    
    * remove clipboardjs from app.js
    
    * move copy button on top of div 'highlight
    
    * move copy button to code block
    
    * adjust copy button position
    
    * remove a comment in doc.js
    
    * remove a useless comment from doc.js
    
    
    Former-commit-id: 49e24776fd31d1c777724d79f3f2d31f1f0a1e27

--- src/js/doc.js
+++ src/js/doc.js
@@ -99,6 +99,16 @@ function processTags() {
 //     })
 // }
 
+//  create copy button
+function addCopy(element) {
+  console.log('element: ', element)
+  var copy = document.createElement('button')
+  copy.className = 'copy'
+  copy.textContent = 'Copy'
+  console.log('copy: ', copy)
+  $(copy).insertBefore(element)
+}
+
 // Process dom elements after loaded
 $(document).ready(function() {
   if ($('.st_tree').length) processStickyTree()
@@ -151,5 +161,27 @@ $(document).ready(function() {
 
     e.preventDefault()
     return false
+  })
+
+  // Copy to Clipboard
+  var $code = document.querySelectorAll('.highlight')
+
+  for (let i = 0; i < $code.length; i++) {
+    addCopy($code[i])
+  }
+
+  var clipboard = new ClipboardJS('.copy', {
+    target: function(trigger) {
+      $('.copy').text('Copy')
+      $('.copy').css('color', '#94a3ea')
+      trigger.innerText = 'Copied'
+      $(trigger).css('color', 'rgb(231, 234, 148)')
+      return trigger.nextElementSibling
+    },
+  })
+
+  // remove text highlight from the selected target text
+  clipboard.on('success', function(e) {
+    e.clearSelection()
   })
 })
