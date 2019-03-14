dede48c635b2fb4c62949056d030901fef307468
leixuechun <xuechunlei922@gmail.com>
2019-03-14 14:53:37 +0800

    Fix/process images in cases cn (#172)
    * chore: process cases-cn images
    
    * chore: minor modify
    
    
    Former-commit-id: 255ea316e87a50b2c08bff0cb6f0f194c83441ae

--- scripts/gen-content.sh
+++ scripts/gen-content.sh
@@ -1,5 +1,5 @@
 node scripts/gen-nav.js 'content/docs/TOC.md' 'data/docs_toc.json' '/docs/'
 node scripts/gen-nav.js 'content/docs-cn/TOC.md' 'data/docs_cn_toc.json' '/docs-cn/'
 node scripts/gen-nav.js 'content/recruit-cn/TOC.md' 'data/recruit_cn_toc.json' '/recruit-cn/'
-node scripts/gen-nav.js 'content/blog-cn/cases/TOC.md' 'data/cases_cn_toc.json' '/cases-cn/'
+node scripts/gen-nav.js 'content/blog-cn/TOC-User-Case.md' 'data/cases_cn_toc.json' '/cases-cn/'
 node scripts/gen-nav.js 'content/tidb-academy/README.md' 'data/tidb_academy_toc.json' '/tidb-academy/'
