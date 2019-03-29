d8c1498b18b3b17e2c292f9f9ff4cbc03d4c7ef0
Yini Xu <34967660+YiniXu9506@users.noreply.github.com>
2019-03-29 11:29:57 +0800

    fix video autoplay on modal (#190)
    * fix video autoplay on modal
    
    * src/js/video_on_modal.js: remove autoPlayVideoInWeXin
    
    
    Former-commit-id: 0d55c9a3b428daf822422e718c0344dfa27d9cd4

--- src/js/video_on_modal.js
+++ src/js/video_on_modal.js
@@ -1,28 +1,28 @@
 const openVideoModal = () => {
   $('.j-video-overlay').fadeIn()
   $('.j-video-overlay, .modal').addClass('active')
-  autoPlayVideoInWeXin()
 }
 
-const autoPlayVideoInWeXin = () => {
-  document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
-    $('#video')[0].play()
-  })
-}
+// const autoPlayVideoInWeXin = () => {
+//   document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+//     $('#video-on-modal')[0].play()
+//   })
+// }
 
 const closeModal = () => {
   $('.modal-overlay').fadeOut()
   $('.modal-overlay, .modal').removeClass('active')
-  $('#video-modal')[0].pause()
+  $('#video-on-modal')[0].pause()
 }
 
 $(function() {
   $('.j-video-btn').on('click', function(e) {
     var theModal = $(this).data('target')
     var videoSRC = $(this).attr('data-video')
-    $('#video-modal').attr('src', videoSRC)
-    $('#video-modal')[0].play()
     openVideoModal()
+    $('#video-on-modal').attr('src', videoSRC)
+    $('#video-on-modal')[0].play()
+    // autoPlayVideoInWeXin()
     e.preventDefault()
     e.stopPropagation()
   })
