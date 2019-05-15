8de8d3e5c2518ebb62cab90ee51996374c55e028
siva <ghlndsl@126.com>
2019-05-15 19:59:05 +0800

    Update build-check-err.sh

--- scripts/build-check-err.sh
+++ scripts/build-check-err.sh
@@ -1,6 +1,6 @@
 gulp build > gulp-build.log; 2>&1;
-if cat gulp-build.log|grep -i 'error'; then
-  echo 'Encouter Error';
+if cat gulp-build.log|grep -i '^error'; then
+  echo 'Found Error in build.log. Exit';
   exit -1;
 else
    bash ./scripts/process-docs.sh;
