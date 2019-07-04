c4123d6808ef67a05771ab32b04f3c6f92d3c712
YiniXu9506 <xuyini@pingcap.com>
2019-07-04 20:56:14 +0800

    add comment in build-check-err.sh

--- scripts/build-check-err.sh
+++ scripts/build-check-err.sh
@@ -1,5 +1,5 @@
 echo "build check err"
-gulp build > gulp-build.log; 2>&1;
+# gulp build > gulp-build.log; 2>&1;
 if cat gulp-build.log|grep -i '^error'; then
   echo 'Found Error in build.log. Exit';
   exit -1;
