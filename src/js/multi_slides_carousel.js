13be1c39164d2b4dbf80a5e27c4ad5cc811518bf
Yini Xu <34967660+YiniXu9506@users.noreply.github.com>
2019-08-12 18:27:00 +0800

    Add mobile carousel (#345)
    * update mobile carousel
    
    * update mobile carousel in homepage

--- src/js/multi_slides_carousel.js
+++ src/js/multi_slides_carousel.js
@@ -1,24 +1,26 @@
 function calcCarousel(slides_per_view, slides_per_group, spaceBetween) {
-  // if (window.matchMedia('(max-width: 600px)').matches && !$('.homepage-slogan-mask').length) {
-  //   var mySwiperThumbs = new Swiper('.swiper-container-thumbs', {
-  //     slidesPerView: 4,
-  //     spaceBetween: 10,
-  //     watchSlidesVisibility: true,
-  //     watchSlidesProgress: true,
-  //     navigation: {
-  //       nextEl: '.swiper-button-next',
-  //       prevEl: '.swiper-button-prev',
-  //     },
-  //   })
-  
-  //   var mySwiper = new Swiper('.swiper-container', {
-  //     spaceBetween: 20,
-  //     thumbs: {
-  //       swiper: mySwiperThumbs,
-  //     }
-  //   })
-  // } else {
-    var mySwiper = new Swiper('.swiper-container', {
+  if (window.matchMedia('(max-width: 600px)').matches && !$('.homepage-slogan-mask').length) {
+    $('.swiper-button-prev').addClass('thumbs-prev')
+    $('.swiper-button-next').addClass('thumbs-next')
+
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+    var galleryTop = new Swiper('.gallery-top', {
+      spaceBetween: 10,
+      thumbs: {
+        swiper: galleryThumbs,
+      },
+    })
+  } else {
+    var mySwiper = new Swiper('.gallery-top', {
       direction: 'horizontal',
       autoplay: {
         delay: 3000,
@@ -38,7 +40,7 @@ function calcCarousel(slides_per_view, slides_per_group, spaceBetween) {
         prevEl: '.swiper-button-prev',
       },
     })
-  // }
+  }
 }
 
 $(document).ready(function() {
@@ -62,5 +64,5 @@ $(document).ready(function() {
 
   calcCarousel(slides_per_view, slides_per_group, spaceBetween)
 
-  $(window).resize(calcCarousel(slides_per_view, slides_per_group, spaceBetween))
+  // $(window).resize(calcCarousel(slides_per_view, slides_per_group, spaceBetween))
 })
