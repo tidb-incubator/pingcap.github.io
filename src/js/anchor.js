6f8da30a6136d1444c2e260595b825c9eb45865b
Yue Yang <g1enyy0ung@gmail.com>
2019-09-17 15:42:35 +0800

    Add anchor button in all submodules' title (#366)

--- src/js/anchor.js
+++ src/js/anchor.js
@@ -2,12 +2,29 @@
   $('.content.markdown-body')
     .find('h1, h2, h3, h4, h5, h6')
     .each(function() {
-      var that = $(this)
-      var newId = that
+      const that = $(this)
+
+      const newId = that
         .text()
         .replace(/\s/g, '-')
         .replace(/[\.\:：,，\?？、&]/g, '')
         .toLowerCase()
       that.attr('id', newId)
+
+      const link = $(
+        `<a class="title-anchor hidden" href="${window.location.href +
+          '#' +
+          newId}"><img src="/images/svgs/link.svg" /></a>`
+      )
+      that.append(link)
+
+      that.hover(
+        function() {
+          link.removeClass('hidden')
+        },
+        function() {
+          link.addClass('hidden')
+        }
+      )
     })
 })()
