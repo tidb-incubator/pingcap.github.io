00b5203541ac29135bfa8061d4d8d421bc6f1d57
Yue Yang <g1enyy0ung@gmail.com>
2019-10-10 17:20:30 +0800

    Fixes duplicative anchors (#374)

--- src/js/anchor.js
+++ src/js/anchor.js
@@ -1,37 +1,51 @@
 ;(function() {
-  $('.content.markdown-body')
-    .find('h1, h2, h3, h4, h5, h6')
-    .each(function() {
-      const that = $(this)
+  const ids = $('.content.markdown-body').find('h1, h2, h3, h4, h5, h6')
+  let idsText = ids.toArray().map(id => {
+    return id.textContent
+  })
+  const idsMap = new Map()
 
-      const newId = that
-        .text()
-        .replace(/\s/g, '-')
-        .replace(/[^-\w\u4E00-\u9FFF]*/g, '')
-        .toLowerCase()
-      that.attr('id', newId)
+  idsText = idsText.map(id => {
+    if (idsMap.has(id)) {
+      const number = idsMap.get(id)
+      idsMap.set(id, number + 1)
+      return `${id}-${number + 1}`
+    } else {
+      idsMap.set(id, 0)
+      return id
+    }
+  })
 
-      const link = $(
-        `<a class="title-anchor hidden" href="${window.location.href.split(
-          '#'
-        )[0] +
-          '#' +
-          newId}"><img src="/images/svgs/link.svg" /></a>`
-      )
-      link.click(function(e) {
-        e.preventDefault()
+  ids.each(function(index) {
+    const that = $(this)
 
-        location.hash = `#${newId}`
-      })
-      that.append(link)
+    const newId = idsText[index]
+      .replace(/\s/g, '-')
+      .replace(/[^-\w\u4E00-\u9FFF]*/g, '')
+      .toLowerCase()
+    that.attr('id', newId)
 
-      that.hover(
-        function() {
-          link.removeClass('hidden')
-        },
-        function() {
-          link.addClass('hidden')
-        }
-      )
+    const link = $(
+      `<a class="title-anchor hidden" href="${window.location.href.split(
+        '#'
+      )[0] +
+        '#' +
+        newId}"><img src="/images/svgs/link.svg" /></a>`
+    )
+    link.click(function(e) {
+      e.preventDefault()
+
+      location.hash = `#${newId}`
     })
+    that.append(link)
+
+    that.hover(
+      function() {
+        link.removeClass('hidden')
+      },
+      function() {
+        link.addClass('hidden')
+      }
+    )
+  })
 })()
