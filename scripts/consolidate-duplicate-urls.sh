2ecd6b6d3c6b54da49f0211f2a398e8920e7eaf0
Yini Xu <34967660+YiniXu9506@users.noreply.github.com>
2019-11-14 16:11:20 +0800

    Update consolidate-duplicate-urls.sh
    update script

--- scripts/consolidate-duplicate-urls.sh
+++ scripts/consolidate-duplicate-urls.sh
@@ -8,6 +8,7 @@ old_versions=(v2.1 dev)
 consolidate_duplicate_urls() {
     local doc_temp_path=$1
     local doc_version=$2
+    echo "executing..."
 
     if [ -d $doc_temp_path ]; then
         for html in $doc_temp_path/*
@@ -20,15 +21,19 @@ consolidate_duplicate_urls() {
                 if [ -f $file_path_in_stable ]; then
                     path=$(dirname ${file_path_in_stable#*/})
                     echo $html
-                    sed -i .bak "s@<\/head>@<link rel=\"canonical\" href=\"https:\/\/pingcap.com\/$path\" \/><\/head>@g" $html
+                    sed -iR "s@<\/head>@<link rel=\"canonical\" href=\"https:\/\/pingcap.com\/$path\" \/><\/head>@g" $html
+                    cat $html | grep "</head>"
+                    ls $doc_temp_path
                 fi
             fi
         done
     fi
 }
 
-for v in ${old_versions[@]}
-do
-    consolidate_duplicate_urls dist/docs-cn/$v $v
-    consolidate_duplicate_urls dist/docs/$v $v
-done
+# for v in ${old_versions[@]}
+# do
+#     consolidate_duplicate_urls dist/docs-cn/$v $v
+#     consolidate_duplicate_urls dist/docs/$v $v
+# done
+
+consolidate_duplicate_urls dist/docs-cn/v2.1/how-to/get-started/explore-sql v2.1
