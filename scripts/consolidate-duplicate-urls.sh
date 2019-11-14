f5404e8a664cf7556afa3bedf1c68e078be80099
Yini Xu <34967660+YiniXu9506@users.noreply.github.com>
2019-11-14 16:26:10 +0800

    Update consolidate-duplicate-urls.sh
    remove R in script

--- scripts/consolidate-duplicate-urls.sh
+++ scripts/consolidate-duplicate-urls.sh
@@ -21,7 +21,7 @@ consolidate_duplicate_urls() {
                 if [ -f $file_path_in_stable ]; then
                     path=$(dirname ${file_path_in_stable#*/})
                     echo $html
-                    sed -iR "s@<\/head>@<link rel=\"canonical\" href=\"https:\/\/pingcap.com\/$path\" \/><\/head>@g" $html
+                    sed -i "s@<\/head>@<link rel=\"canonical\" href=\"https:\/\/pingcap.com\/$path\" \/><\/head>@g" $html
                     cat $html | grep "</head>"
                     ls $doc_temp_path
                 fi
@@ -30,10 +30,10 @@ consolidate_duplicate_urls() {
     fi
 }
 
-# for v in ${old_versions[@]}
-# do
-#     consolidate_duplicate_urls dist/docs-cn/$v $v
-#     consolidate_duplicate_urls dist/docs/$v $v
-# done
+for v in ${old_versions[@]}
+do
+    consolidate_duplicate_urls dist/docs-cn/$v $v
+    consolidate_duplicate_urls dist/docs/$v $v
+done
 
-consolidate_duplicate_urls dist/docs-cn/v2.1/how-to/get-started/explore-sql v2.1
+# consolidate_duplicate_urls dist/docs-cn/v2.1/how-to/get-started/explore-sql v2.1
