01dee39faec86ff0c8a4e071bb61972a30339214
Yini Xu <34967660+YiniXu9506@users.noreply.github.com>
2020-04-16 16:05:53 +0800

    fix: remove outside images in dist (#458)

--- scripts/process-docs.sh
+++ scripts/process-docs.sh
@@ -77,3 +77,25 @@ echo "end time" $end_time
 replace_dist_html_link "dist/cases-cn" blog-cn
 replace_dist_html_link "dist/success-stories" blog
 echo "finish all replacement"
+
+parent_dir="`echo $(pwd) | sed 's;/scripts;;g'`/dist"
+rm_images_from_media_docs() {
+  repo_name=$1
+  rm -rf $parent_dir/$repo_name/media
+}
+
+# mv all content in media to dist/images
+rm_images_from_media_docs docs/v2.1
+rm_images_from_media_docs docs/dev
+rm_images_from_media_docs docs/stable
+rm_images_from_media_docs docs/v3.1
+rm_images_from_media_docs docs/v3.0
+rm_images_from_media_docs docs-cn/v2.1
+rm_images_from_media_docs docs-cn/dev
+rm_images_from_media_docs docs-cn/stable
+rm_images_from_media_docs docs-cn/v3.1
+rm_images_from_media_docs docs-cn/v3.0
+rm_images_from_media_docs blog-cn
+rm_images_from_media_docs blog
+rm_images_from_media_docs weekly
+rm_images_from_media_docs meetup
