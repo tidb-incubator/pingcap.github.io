1c7d7f607e501c53071af166ca1d566720420c3e
Yini Xu <34967660+YiniXu9506@users.noreply.github.com>
2020-12-21 17:59:13 +0800

    chore: update webpack entries (#535)
    * chore: remove object.fromEntries since it's not supported on node.js ver 8.x
    
    * chore: update output file name
    
    * chore: optimize entry object  code

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,22 @@ const glob = require('glob')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-
 const isDev = process.env.NODE_ENV === 'development'
-const srcDir = path.join(__dirname, 'src', 'js')
-const entries = glob
-  .sync('*.js', {
-    cwd: srcDir,
-  })
-  .map(key => [key.replace('.js', ''), path.resolve(srcDir, key)])
-const entryObj = Object.fromEntries(entries)
+
+// dynamic add js files to entry object
+const entryObj = glob
+  .sync(path.join(__dirname, './src/js/*.js'))
+  .reduce((entries, path) => {
+    let arr = path.split('/')
+    entries[arr[arr.length - 1]] = path
+    return entries
+  }, {})
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
   entry: entryObj,
   output: {
+    filename: '[name]',
     path: path.join(__dirname, 'dist', 'js'),
     publicPath: '/js/',
   },
