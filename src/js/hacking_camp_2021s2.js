302965355893c4d85e04b1bfba14ab9cfab645eb
Nick Qi <wei.qi.nick@gmail.com>
2021-06-27 16:34:37 +1200

    CP-39 refactor: add placeholder files for the hacking camp

---
+++ src/js/hacking_camp_2021s2.js
@@ -0,0 +1,121 @@
+function processNavbar() {
+  const hash = decodeURIComponent(location.hash)
+  if (!hash) return
+
+  const navItem = $('.navbar__item a').filter(function() {
+    return $(this).attr('href') === hash
+  })
+
+  if (!navItem) return
+
+  navItem.parent().addClass('active')
+}
+
+function closeModal() {
+  $('.modal-overlay').fadeOut()
+  $('.modal-overlay, .modal').removeClass('active')
+}
+
+function processDate() {
+  const date = new Date()
+  const today =
+    date.getFullYear() + '.' + (date.getMonth() + 1) + '.' + date.getDate()
+  const milestones = ['2021.1.10', '2021.1.11', '2021.1.16', '2021.1.17']
+  const hackingCampMilestones = [
+    '2021.3.14',
+    '2021.3.21',
+    '2021.4.26',
+    '2021.4.29',
+  ]
+  const moments = $('.hackathon-moment-item')
+  const hackingMoments = $('.hacking-camp-moment')
+
+  if (today <= milestones[0]) {
+    // ~ 2020.1.10
+    $(moments[0]).addClass('active')
+  } else if (today < milestones[2]) {
+    // 2021.1.11 ~ 2021.1.15
+    $(moments[1]).addClass('active')
+  } else if (today < milestones[3]) {
+    // 2021.1.16 ~ 2021.1.17
+    $(moments[2]).addClass('active')
+  } else {
+    // 2021.1.17 ~
+    $(moments[3]).addClass('active')
+  }
+
+  if (today <= hackingCampMilestones[0]) {
+    // ~ 2021.02.23
+    $(hackingMoments[0]).addClass('active')
+  } else if (today < hackingCampMilestones[2]) {
+    // 2021.03.15 ~ 2021.03.20
+    $(hackingMoments[1]).addClass('active')
+  } else if (today < hackingCampMilestones[3]) {
+    // 2021.04.20 ~ 2021.04.25
+    $(hackingMoments[2]).addClass('active')
+  } else {
+    // 2021.04.28 ~
+    $(hackingMoments[3]).addClass('active')
+  }
+}
+
+$(document).ready(function() {
+  // auto active nav item with hash
+  processNavbar()
+
+  // auto active current milestone in moments
+  processDate()
+
+  $('.navbar__item').click(function() {
+    $(this)
+      .parent()
+      .children('.navbar__item')
+      .removeClass('active')
+
+    $(this).addClass('active')
+  })
+
+  // toggle answers when click question
+  $('.question').click(function() {
+    $(this)
+      .parent()
+      .siblings()
+      .children('.question')
+      .removeClass('expand')
+    $(this)
+      .next()
+      .slideToggle()
+    $(this).toggleClass('expand')
+    $(this)
+      .parent()
+      .siblings()
+      .children('.answer')
+      .slideUp()
+  })
+
+  $('.j-open-agenda-modal').click(function() {
+    $('.j-agenda-overlay').fadeIn()
+    $('.j-agenda-overlay, .modal').addClass('active')
+  })
+
+  $('.j-open-criteria-modal').click(function() {
+    $('.j-criteria-overlay').fadeIn()
+    $('.j-criteria-overlay, .modal').addClass('active')
+  })
+
+  $('.modal-overlay').on('click', function(e) {
+    if ($(this).hasClass('active')) {
+      if (e.target === e.delegateTarget) {
+        closeModal()
+      }
+    }
+  })
+
+  $('.close-modal')
+    .off('click')
+    .on('click', function(e) {
+      e.preventDefault()
+      e.stopPropagation()
+      closeModal()
+    })
+})
--- webpack.config.js
+++ webpack.config.js
@@ -6,29 +6,39 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const isDev = process.env.NODE_ENV === 'development'
 const pathJoinSrcJs = name => path.join(__dirname, 'src', 'js', name)
 
+const generateEntry = filenames =>
+  filenames.reduce(
+    (acc, filename) => ({
+      ...acc,
+      [filename]: pathJoinSrcJs(filename),
+    }),
+    {}
+  )
+
 module.exports = {
   mode: isDev ? 'development' : 'production',
-  entry: {
-    app: pathJoinSrcJs('app'),
-    doc: pathJoinSrcJs('doc'),
-    about: pathJoinSrcJs('about'),
-    recruit: pathJoinSrcJs('recruit'),
-    tidb_planet: pathJoinSrcJs('tidb_planet'),
-    devcon: pathJoinSrcJs('devcon'),
-    community: pathJoinSrcJs('community'),
-    tidb_planet_user: pathJoinSrcJs('tidb_planet_user'),
-    multi_slides_carousel: pathJoinSrcJs('multi_slides_carousel'),
-    carousel: pathJoinSrcJs('carousel'),
-    video_on_modal: pathJoinSrcJs('video_on_modal'),
-    anchor: pathJoinSrcJs('anchor'),
-    tidb_performance_challenge: pathJoinSrcJs('tidb_performance_challenge'),
-    tidb_usability_challenge: pathJoinSrcJs('tidb_usability_challenge'),
-    tidb_4_challenge: pathJoinSrcJs('tidb_4_challenge'),
-    high_performance_tidb_challenge: pathJoinSrcJs('high_performance_tidb_challenge'),
-    developer_sig: pathJoinSrcJs('developer_sig'),
-    hackathon2020: pathJoinSrcJs('hackathon2020'),
-    devcon2021: pathJoinSrcJs('devcon2021'),
-  },
+  entry: generateEntry([
+    'about',
+    'anchor',
+    'app',
+    'carousel',
+    'community',
+    'devcon',
+    'devcon2021',
+    'developer_sig',
+    'doc',
+    'hackathon2020',
+    'hacking_camp_2021s2',
+    'high_performance_tidb_challenge',
+    'multi_slides_carousel',
+    'recruit',
+    'tidb_4_challenge',
+    'tidb_performance_challenge',
+    'tidb_planet',
+    'tidb_planet_user',
+    'tidb_usability_challenge',
+    'video_on_modal',
+  ]),
   output: {
     path: path.join(__dirname, 'dist', 'js'),
     publicPath: '/js/',
