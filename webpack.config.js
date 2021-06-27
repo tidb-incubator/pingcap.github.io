3432ed1a095bc922cc9af868c41beda026c2a2c6
Nick Qi <wei.qi.nick@gmail.com>
2021-06-27 16:58:18 +1200

    CP-39 refactor: auto generate the entry files for the webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,32 @@
-const path = require('path')
 const glob = require('glob')
+const path = require('path')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
 const isDev = process.env.NODE_ENV === 'development'
 const pathJoinSrcJs = name => path.join(__dirname, 'src', 'js', name)
 
-const generateEntry = filenames =>
-  filenames.reduce(
+const generateEntry = () => {
+  const filenames = glob.sync('src/js/*.js').map(path =>
+    path
+      .split('/')
+      .pop()
+      .replace(/.js$/i, '')
+  )
+
+  return filenames.reduce(
     (acc, filename) => ({
       ...acc,
       [filename]: pathJoinSrcJs(filename),
     }),
     {}
   )
+}
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
-  entry: generateEntry([
-    'about',
-    'anchor',
-    'app',
-    'carousel',
-    'community',
-    'devcon',
-    'devcon2021',
-    'developer_sig',
-    'doc',
-    'hackathon2020',
-    'hacking_camp_2021s2',
-    'high_performance_tidb_challenge',
-    'multi_slides_carousel',
-    'recruit',
-    'tidb_4_challenge',
-    'tidb_performance_challenge',
-    'tidb_planet',
-    'tidb_planet_user',
-    'tidb_usability_challenge',
-    'video_on_modal',
-  ]),
+  entry: generateEntry(),
   output: {
     path: path.join(__dirname, 'dist', 'js'),
     publicPath: '/js/',
