6c5a6d639048704268ef7cdc6f0fa49566669dbe
std4453 <me@std4453.com>
2021-06-28 17:34:42 +0800

    CPT-262: migrate event pages (#1)
    * CPT 262 feat: introduce bundled @tidb-community/ui for header and footer
    
    * CPT-262 feat: update baseof.tpl to match online scripts
    
    * CPT-264 feat: add test CI config
    
    * CPT-265 feat: update meetup url path
    
    * Add .circleci/config.yml
    
    * CPT-264 fix: fix CI config #1
    
    * CPT-264 fix: fix CI config #2
    
    * CPT-254 fix: fix CI config #3
    
    * CPT-264 fix: fix CI config #4
    
    * CPT-265 feat: update meetup and events url
    
    * CPT-264 feat: fix shipit config
    
    * CPT_265 feat: migrate to archive/
    
    * CPT-264: remove http-server to host with nginx
    
    * CPT-262 fix: remove favicon
    
    * CPT-262 fix: use favicon of tidb.io
    
    * CPT-262 fix: update other image urls
    
    * CPT-262 fix: don't fetch API for data
    
    * CPT-262 fix: wrong url of recordings page
    
    * CPT-262 feat: merge upstream/src into CPT-262-migrate-event-pages
    
    commit d6f8eb22e5d4796d82920bba34f526d535cac7bb
    Author: YiniXu9506 <xuyini@pingcap.com>
    Date:   Mon Jun 28 14:40:51 2021 +0800
    
        chore: update docs old version to stable version
    
    commit a1abe7b6114c5752da254dafccd90040f60a4f0f
    Author: Yue Yang <g1enyy0ung@gmail.com>
    Date:   Sat Jun 26 21:44:58 2021 +0800
    
        chore: use stable quick start
    
    commit a31a8c901a1450a8f0bf2df4c701533d968a8f30
    Author: YiniXu9506 <xuyini@pingcap.com>
    Date:   Thu Jun 24 19:13:46 2021 +0800
    
        chore: add new download tidb package
    
    commit acf659341c0d96999511507ddf23ca51228a5dc4
    Author: YiniXu9506 <xuyini@pingcap.com>
    Date:   Thu Jun 24 12:02:49 2021 +0800
    
        chore: update sponsor devcon 2021
    
    commit 3b419cbd47e4ed0976fbeca43977ab9019f2d39c
    Author: YiniXu9506 <xuyini@pingcap.com>
    Date:   Wed Jun 23 16:45:24 2021 +0800
    
        chore: update devcon2021 banner
    
    commit f106af29822764e44c4efafabacfe886ba01b2b9
    Author: Yini Xu <34967660+YiniXu9506@users.noreply.github.com>
    Date:   Tue Jun 22 21:03:01 2021 +0800
    
        Add DevCon 2021 campaign (#555)
    
        * chore: add devcon 2021 campaign
    
        * chore: update devcon2021
    
        * chore: update ui
    
        * chore: update devcon2021
    
        * chore: update devcon2021
    
        * chore: update devcon2021
    
        * chore: update devcon2021
    
    * CPT-262 fix: fix devcon2021 link
    
    * CPT-262 fix: fix images in devcon2021
    
    * CPT-262 fix: fix images in devon2020 #2
    
    * CPT-262 fix: fix images in devcon2021 #3
    
    * CPT-262 fix: fix other images
    
    * CPT-265 feat: restore CI branch to src

---
+++ shipitfile.js
@@ -0,0 +1,34 @@
+// Learnt from:
+// https://git.vnv.ch/snippets/17#more-documentations-options
+// https://www.digitalocean.com/community/tutorials/how-to-automate-your-node-js-production-deployments-with-shipit-on-centos-7
+module.exports = shipit => {
+  require('shipit-deploy')(shipit)
+
+  shipit.initConfig({
+    default: {
+      workspace: '.',
+      deployTo: process.env.DEPLOY_SERVICE_PATH,
+
+      // The node_modules of packages are not sent since they've been bundled in the
+      // main server. The root node_modules is still needed for starting the app.
+      ignores: ['.git'],
+      keepReleases: 3,
+
+      // The workspace dir won't be removed after deploy
+      keepWorkspace: true,
+
+      // Disable the setup of Git repository (fetch, pull, merge, submodules, etc.)
+      // because the current context is already up to date(CI/ CD)
+      shallowClone: false,
+      branch: 'HEAD',
+      copy: false,
+    },
+
+    production: {
+      servers: {
+        user: process.env.HOST_HK_1_USER,
+        host: process.env.HOST_HK_1_IP,
+      },
+    },
+  })
+}
