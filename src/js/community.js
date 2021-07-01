d70d5c199179bdb1faa0d37981ee8f8578dadb40
Nick Qi <wei.qi.nick@gmail.com>
2021-07-01 20:38:34 +1200

    CP-39 feat: update text

--- src/js/community.js
+++ src/js/community.js
@@ -269,7 +269,6 @@ $(document).ready(function() {
     }
   })
 
-  console.log('test!!', window.matchMedia('(max-width: 414px)').matches)
   if (window.matchMedia('(max-width: 414px)').matches) {
     $('.j-thumbnailText-click').click(function() {
       if ($('.thumbnail-text').hasClass('thumbnail-text-click')) {
